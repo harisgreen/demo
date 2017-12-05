c48568b2ec54cd2f9114316cd62cbadd5e998ac7
Moiez Siddiqui <k092107@nu.edu.pk>
2017-12-05 20:57:19 +0500

    hello

--- src/main/java/com/example/demo/MongotestMainApplication.java
+++ src/main/java/com/example/demo/MongotestMainApplication.java
@@ -1,9 +1,11 @@
 package com.example.demo;
 
+
 import org.jsondoc.spring.boot.starter.EnableJSONDoc;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.mongodb.config.EnableMongoAuditing;
+import org.springframework.data.mongodb.core.aggregation.ConditionalOperators;
 
 @SpringBootApplication
 @EnableMongoAuditing
@@ -11,6 +13,45 @@ import org.springframework.data.mongodb.config.EnableMongoAuditing;
 public class MongotestMainApplication {
 
 	public static void main(String[] args) {
+		Integer i = 1000;
+
+		System.out.println("hello");
+		while (i > 1) {
+			i--;
+			if (1 == 1) {
+				if (2 == 2) {
+					if (3 == 3) {
+						if (4 == 4) {
+
+							for(int ii = 0; ii < i ;ii++){
+								System.out.println(ii + "is here");
+							}
+						}else{
+							for(int ii = 0; ii < i ;ii++){
+								System.out.println(ii + "is here");
+							}
+						}
+					}
+				}
+			}
+		}
+
+
+
+		if (1 == 1) {
+			if (2 == 2) {
+				if (3 == 3) {
+					if (4 == 4) {
+						System.out.println( "we are here");
+
+					}else{
+						System.out.println( "we are not here");
+					}
+				}
+			}
+		}
+
+
 
 		SpringApplication.run(MongotestMainApplication.class, args);
 	}
