a3bbc5d83bce5922f057e54fd8f5b9b41b0f1011
Moiez Siddiqui <k092107@nu.edu.pk>
2017-12-12 20:25:03 +0500

    ahmed

--- src/test/java/com/example/demo/MongotestMainApplicationTests.java
+++ src/test/java/com/example/demo/MongotestMainApplicationTests.java
@@ -11,6 +11,63 @@ public class MongotestMainApplicationTests {
 
 	@Test
 	public void contextLoads() {
+		int i56 = 42;
+
+		for (int i = 0; i < 10; i56++) {  // Noncompliant
+			System.out.println("hello");
+		}
+	}
+
+	@Test
+	public void contextLoads2() {
+
+		int matrix1[][];   // Noncompliant
+		int[] matrix[];
+	}
+
+
+	@Test
+	public void contextLoads3() {
+		int i56 = 42;
+		double d = Double.longBitsToDouble(i56);
+		System.out.println("hello");
+		int i = 52;
+		while (i > 1) {
+			i--;
+			if (1 == 1) {
+				if (2 == 2) {
+					if (3 == 3) {
+						if (4 == 4) {
+
+							for(int ii = 0; ii < i ;ii++){
+								System.out.println(ii + "is here");
+							}
+						}else{
+							for(int ii = 0; ii < i ;ii++){
+								System.out.println(ii + "is here");
+							}
+						}
+					}
+				}
+			}
+		}
+	}
+
+	@Test
+	public void contextLoads4() {
+
+		if (1 == 1) {
+			if (2 == 2) {
+				if (3 == 3) {
+					if (4 == 4) {
+						System.out.println( "we are here");
+
+					}else{
+						System.out.println( "we are not here");
+					}
+				}
+			}
+		}
 	}
 
 }
