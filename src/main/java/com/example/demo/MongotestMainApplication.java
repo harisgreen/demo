96af361d365beae2410696b831d35eb806e2b4aa
Moiez Siddiqui <k092107@nu.edu.pk>
2017-12-12 19:40:52 +0500

    heloo2

--- src/main/java/com/example/demo/MongotestMainApplication.java
+++ src/main/java/com/example/demo/MongotestMainApplication.java
@@ -14,6 +14,174 @@ public class MongotestMainApplication {
 
 	public static void main(String[] args) {
 
+		Integer i = 1000;
+
+		System.out.println("hello");
+		while (i > 1) {
+			i--;
+			if (1 == 1) {
+				if (2 == 2) {
+					if (3 == 3) {
+						if (4 == 4) {
+
+							for(int ii = 0; ii < i ;ii++){
+								System.out.println(ii + "is here");
+							}
+						}else{
+							for(int ii = 0; ii < i ;ii++){
+								System.out.println(ii + "is here");
+							}
+						}
+					}
+				}
+			}
+		}
+
+
+		while (i > 1) {
+			i--;
+			if (1 == 1) {
+				if (2 == 2) {
+					if (3 == 3) {
+						if (4 == 4) {
+
+							for(int ii = 0; ii < i ;ii++){
+								System.out.println(ii + "is here");
+							}
+						}else{
+							for(int ii = 0; ii < i ;ii++){
+								System.out.println(ii + "is here");
+							}
+						}
+					}
+				}
+			}
+		}
+
+
+		while (i > 1) {
+			i--;
+			if (1 == 1) {
+				if (2 == 2) {
+					if (3 == 3) {
+						if (4 == 4) {
+
+							for(int ii = 0; ii < i ;ii++){
+								System.out.println(ii + "is here");
+							}
+						}else{
+							for(int ii = 0; ii < i ;ii++){
+								System.out.println(ii + "is here");
+							}
+						}
+					}
+				}
+			}
+		}
+
+
+		while (i > 1) {
+			i--;
+			if (1 == 1) {
+				if (2 == 2) {
+					if (3 == 3) {
+						if (4 == 4) {
+
+							for(int ii = 0; ii < i ;ii++){
+								System.out.println(ii + "is here");
+							}
+						}else{
+							for(int ii = 0; ii < i ;ii++){
+								System.out.println(ii + "is here");
+							}
+						}
+					}
+				}
+			}
+		}
+
+
+		if (1 == 1) {
+			if (2 == 2) {
+				if (3 == 3) {
+					if (4 == 4) {
+						System.out.println( "we are here");
+
+					}else{
+						System.out.println( "we are not here");
+					}
+				}
+			}
+		}
+
+		if (1 == 1) {
+			if (2 == 2) {
+				if (3 == 3) {
+					if (4 == 4) {
+						System.out.println( "we are here");
+
+					}else{
+						System.out.println( "we are not here");
+					}
+				}
+			}
+		}
+
+
+		if (1 == 1) {
+			if (2 == 2) {
+				if (3 == 3) {
+					if (4 == 4) {
+						System.out.println( "we are here");
+
+					}else{
+						System.out.println( "we are not here");
+					}
+				}
+			}
+		}
+
+
+		if (1 == 1) {
+			if (2 == 2) {
+				if (3 == 3) {
+					if (4 == 4) {
+						System.out.println( "we are here");
+
+					}else{
+						System.out.println( "we are not here");
+					}
+				}
+			}
+		}
+
+
+		if (1 == 1) {
+			if (2 == 2) {
+				if (3 == 3) {
+					if (4 == 4) {
+						System.out.println( "we are here");
+
+					}else{
+						System.out.println( "we are not here");
+					}
+				}
+			}
+		}
+
+
+		if (1 == 1) {
+			if (2 == 2) {
+				if (3 == 3) {
+					if (4 == 4) {
+						System.out.println( "we are here");
+
+					}else{
+						System.out.println( "we are not here");
+					}
+				}
+			}
+		}
 
 		SpringApplication.run(MongotestMainApplication.class, args);
 	}
